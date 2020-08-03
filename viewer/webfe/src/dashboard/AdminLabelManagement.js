1e4f151a672204eb2746274f16569cdc947c0b25
Cryptosis <j4sumskas@gmail.com>
2020-08-03 15:03:57 +1000

    Trivial: added margin to label management pages
    Tested-by: Joshua <j4sumskas@gmail.com>
    Reviewed-by: Chester Pang <i@bopa.ng>
    GitOrigin-RevId: d67e3e55faaf7326d5c0b43e3c60bb7bef725108

--- viewer/webfe/src/dashboard/AdminLabelManagement.js
+++ viewer/webfe/src/dashboard/AdminLabelManagement.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
 
+import { makeStyles } from "@material-ui/core/styles";
+
 import MaterialTable from "material-table";
 import TableContainer from "@material-ui/core/TableContainer";
 import Paper from "@material-ui/core/Paper";
+import Container from "@material-ui/core/Container";
 
 import Search from "@material-ui/icons/Search";
 import ChevronLeft from "@material-ui/icons/ChevronLeft";
@@ -25,12 +28,21 @@ import {
 import { DataAggregator } from "productimon/proto/svc/aggregator_pb_service";
 import { Label } from "productimon/proto/common/common_pb";
 
+const useStyles = makeStyles((theme) => ({
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+    height: "90%",
+  },
+}));
+
 function createData(program, label, numUsers) {
   return { program, label, numUsers };
 }
 
 export default function AdminLabelManagement(props) {
   const [data, setData] = useState([]);
+  const classes = useStyles();
 
   let columns = [
     { title: "Program", field: "program", editable: "never" },
@@ -62,52 +74,54 @@ export default function AdminLabelManagement(props) {
   }, [props.admin]);
 
   return (
-    <TableContainer component={Paper}>
-      <MaterialTable
-        title={props.admin ? "Server Labels" : "My Labels"}
-        columns={columns}
-        data={data}
-        localization={{
-          header: {
-            actions: "Edit Label",
-          },
-        }}
-        icons={{
-          Check: Check,
-          DetailPanel: ChevronRight,
-          FirstPage: FirstPage,
-          LastPage: LastPage,
-          NextPage: ChevronRight,
-          PreviousPage: ChevronLeft,
-          Search: Search,
-          ThirdStateCheck: Remove,
-          Edit: EditIcon,
-          Clear: ClearIcon,
-          ResetSearch: BackspaceIcon,
-          SortArrow: ArrowUpwardIcon,
-        }}
-        options={{
-          actionsColumnIndex: -1,
-          pageSizeOptions: [10, 20, 50],
-          pageSize: 10,
-        }}
-        editable={{
-          onRowUpdate: (newData, oldData) => {
-            if (newData.label == "") newData.label = "Unknown";
-            const label = new Label();
-            label.setApp(newData.program);
-            label.setLabel(newData.label);
-            const request = new DataAggregatorUpdateLabelRequest();
-            request.setAllLabels(props.admin);
-            request.setLabel(label);
-            return rpc(DataAggregator.UpdateLabel, request).then((res) => {
-              setData(
-                data.map((d) => (d.program == newData.program ? newData : d))
-              );
-            });
-          },
-        }}
-      />
-    </TableContainer>
+    <Container maxWidth="lg" className={classes.container}>
+      <TableContainer component={Paper}>
+        <MaterialTable
+          title={props.admin ? "Server Labels" : "My Labels"}
+          columns={columns}
+          data={data}
+          localization={{
+            header: {
+              actions: "Edit Label",
+            },
+          }}
+          icons={{
+            Check: Check,
+            DetailPanel: ChevronRight,
+            FirstPage: FirstPage,
+            LastPage: LastPage,
+            NextPage: ChevronRight,
+            PreviousPage: ChevronLeft,
+            Search: Search,
+            ThirdStateCheck: Remove,
+            Edit: EditIcon,
+            Clear: ClearIcon,
+            ResetSearch: BackspaceIcon,
+            SortArrow: ArrowUpwardIcon,
+          }}
+          options={{
+            actionsColumnIndex: -1,
+            pageSizeOptions: [10, 20, 50],
+            pageSize: 10,
+          }}
+          editable={{
+            onRowUpdate: (newData, oldData) => {
+              if (newData.label == "") newData.label = "Unknown";
+              const label = new Label();
+              label.setApp(newData.program);
+              label.setLabel(newData.label);
+              const request = new DataAggregatorUpdateLabelRequest();
+              request.setAllLabels(props.admin);
+              request.setLabel(label);
+              return rpc(DataAggregator.UpdateLabel, request).then((res) => {
+                setData(
+                  data.map((d) => (d.program == newData.program ? newData : d))
+                );
+              });
+            },
+          }}
+        />
+      </TableContainer>
+    </Container>
   );
 }
